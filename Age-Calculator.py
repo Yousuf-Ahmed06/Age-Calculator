e1a9cb94cd0ff04c0dc595a31fefce3f52ee922c
Yousuf Ahmed <86107195+Yousuf-Ahmed06@users.noreply.github.com>
2024-08-12 23:24:12 +0500

    Update Age-Calculator.py

--- Age-Calculator.py
+++ Age-Calculator.py
@@ -1,5 +1,5 @@
 print("welcome to age calculator")
 DOB = int(input("please enter your birth year"))
-current_year = 2023
+current_year = 2024
 age = current_year - DOB
 print("you are", age, "year old")
